90dbabc4039fdfbe4957b84f8ee60dbe16d6987e
XPRESSDRAFT <info@xpressdraft.com.au>
2026-06-12 21:57:22 +1000

    Add files via upload

--- server/twilio.js
+++ server/twilio.js
@@ -19,7 +19,7 @@ async function sendProposalSMS(clientName, clientPhone, siteAddress, repName, re
   if (!phone.startsWith('+')) phone = '+61' + phone;
 
   const contactLine = repPhone ? `please contact ${repName} directly on ${repPhone}` : `please feel free to contact us`;
-  const message = `Hi ${clientName.split(' ')[0]}, this message is to let you know that we have sent you the proposal for your project. Should you have any questions or require any clarification, ${contactLine}. We would be more than happy to assist. Have a lovely day. Sincerely, The Xpressdraft Team. NO REPLY.`;
+  const message = `XPRESSDRAFT - Hi ${clientName.split(' ')[0]}, this message is to let you know that we have sent you the proposal for your project. Should you have any questions or require any clarification, ${contactLine}. We would be more than happy to assist. Have a lovely day. Sincerely, The Xpressdraft Team. NO REPLY.`;
 
   try {
     const client = getClient();
