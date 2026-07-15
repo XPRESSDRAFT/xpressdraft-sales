a51e73a1d168ab32d14e350ed7795eccdd178a1a
XPRESSDRAFT PTY LTD <info@xpressdraft.com.au>
2026-07-15 19:29:35 +1000

    Add files via upload

--- server/monday.js
+++ server/monday.js
@@ -5,7 +5,7 @@ const MONDAY_API = 'https://api.monday.com/v2';
 
 // Board IDs
 const BOARDS = {
-  negotiations:      '224210864',
+  negotiations:      '18390237344',
   free_consultations: '224212751',
   proposal:          '18389820785',
 };
